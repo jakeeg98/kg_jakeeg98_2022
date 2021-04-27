f8f11d3bc58a8f93acafbb4d0fde1133455c8e9d
jakeeg98 <jakeegermano@gmail.com>
2021-04-27 14:26:54 -0500

    set up file, parsed arguments and created dictionary for number to word mapping

---
+++ main.js
@@ -0,0 +1,13 @@
+var myArgs = process.argv.slice(2);
+
+var pairs = {"0": "Zero", "1" : "One", "2" : "Two", "3" : "Three", "4" : "Four", "5" : "Five", "6" : "Six", "7" : "Seven",
+"8" : "Eight", "9" : "Nine"};
+
+myArgs.map(String);
+
+var arrOut = [];
+
+
+
+
+
